ec19c0cc97d2236c41c86fb92f650b76c55b9f1a
Diana Garcia <dianasgr12@gmail.com>
2026-09-18 10:43:02 -0600

    Eliminar color neon en logo y se borra css que no se ocupa

--- javascript/globalElements.js
+++ javascript/globalElements.js
@@ -25,9 +25,9 @@
 const mainNavbar = document.querySelector(".navThePixelVault");
 
 const renderNavbar = () => {
-    // Solo metemos los tres bloques hijos directamente. 
-    // Así, el flex-direction: row-reverse de tu CSS actúa sobre ellos de inmediato.
-    let botonMenu =`<!-- ========== MENÚ RADIAL DANIEL ========== -->
+  // Solo metemos los tres bloques hijos directamente. 
+  // Así, el flex-direction: row-reverse de tu CSS actúa sobre ellos de inmediato.
+  let botonMenu = `<!-- ========== MENÚ RADIAL DANIEL ========== -->
                     <div id="radialMenuOverlay" class="radial-overlay hidden">
                       <div class="radial-menu">
                         
@@ -72,7 +72,7 @@ const renderNavbar = () => {
                     </div>
                     <!-- ========== FINAL MENÚ RADIAL DANIEL ========== -->`
 
-    const contenidoInterno = `
+  const contenidoInterno = `
     <div class="contenedormenu">
         <div class="menuinteractivo" id="menuinteractivo">
             <div class="ranura" id="ranura"></div>
@@ -86,13 +86,13 @@ const renderNavbar = () => {
             </div>
         </div>
     </div>
-    <img class="logo magicalGlowLogo" src="assets/ThePixelVaultLogo.png">
+    <img class="logo" src="assets/ThePixelVaultLogo.png">
     <div class="contenedorBarras">
         <img class="barraRosa" src="assets/navBarraRosa.png">
         <img class="barraAzul" src="assets/navBarraAzul.png">
     </div>`;
-    
-    mainNavbar.insertAdjacentHTML("beforeend", contenidoInterno);
+
+  mainNavbar.insertAdjacentHTML("beforeend", contenidoInterno);
 };
 renderNavbar();
 
@@ -105,7 +105,7 @@ const ranura = document.querySelector('#ranura');
 const menudesplegable = document.querySelector('#menudesplegable');
 const dropSound = new Audio(`assets/sonidomoneda.mp3`);
 
-let interactuando = false; 
+let interactuando = false;
 let agarreX, agarreY; // Coordenadas del desfase del cursor al hacer clic
 
 // Coordenadas iniciales fijas de la moneda
@@ -118,37 +118,37 @@ let posicionactualY = inicioY;
 
 //* Evento POINTERDOWN
 moneda.addEventListener("pointerdown", (e) => {
-    interactuando = true; // (Sin 'let' para modificar la variable global)
-    moneda.setPointerCapture(e.pointerId);
-    
-    // Calculamos el desfase exacto donde hizo clic el usuario dentro de la moneda
-    agarreX = e.clientX - posicionactualX;
-    agarreY = e.clientY - posicionactualY;
+  interactuando = true; // (Sin 'let' para modificar la variable global)
+  moneda.setPointerCapture(e.pointerId);
+
+  // Calculamos el desfase exacto donde hizo clic el usuario dentro de la moneda
+  agarreX = e.clientX - posicionactualX;
+  agarreY = e.clientY - posicionactualY;
 });
 
 //* Evento POINTERMOVE
 moneda.addEventListener("pointermove", (e) => {
-    if (!interactuando) return; 
+  if (!interactuando) return;
 
-    // Calcula las nuevas coordenadas restando la posición actual del cursor menos el desfase
-    posicionactualX = e.clientX - agarreX;
-    posicionactualY = e.clientY - agarreY;
-    
-    // Se actualiza la posición en tiempo real
-    moneda.style.left = `${posicionactualX}px`;
-    moneda.style.top = `${posicionactualY}px`;
+  // Calcula las nuevas coordenadas restando la posición actual del cursor menos el desfase
+  posicionactualX = e.clientX - agarreX;
+  posicionactualY = e.clientY - agarreY;
+
+  // Se actualiza la posición en tiempo real
+  moneda.style.left = `${posicionactualX}px`;
+  moneda.style.top = `${posicionactualY}px`;
 });
 
 //* Evento POINTERUP 
 moneda.addEventListener("pointerup", (e) => {
-    if (!interactuando) return;
-    interactuando = false;
+  if (!interactuando) return;
+  interactuando = false;
 
-    try {
-        moneda.releasePointerCapture(e.pointerId);
-    } catch (error) {}
+  try {
+    moneda.releasePointerCapture(e.pointerId);
+  } catch (error) { }
 
-    checkDropZone(); // Llama a la función que verifica si cayó dentro de la zona de ranura
+  checkDropZone(); // Llama a la función que verifica si cayó dentro de la zona de ranura
 });
 
 //* CheckDropZone
@@ -159,84 +159,84 @@ function checkDropZone() {
 
   // Calcula si hay colisión entre las coordenadas de la moneda y la ranura
   const overlap = !(
-      monedaRect.right < ranuraRect.left || 
-      monedaRect.left > ranuraRect.right || 
-      monedaRect.bottom < ranuraRect.top || 
-      monedaRect.top > ranuraRect.bottom
+    monedaRect.right < ranuraRect.left ||
+    monedaRect.left > ranuraRect.right ||
+    monedaRect.bottom < ranuraRect.top ||
+    monedaRect.top > ranuraRect.bottom
   );
 
   //* IF ELSE para los eventos si cayó o no dentro de la ranura
   if (overlap) {
-      moneda.style.pointerEvents = 'none';
-      
-      // Activa sonido
-      dropSound.currentTime = 0;
-      dropSound.play().catch(e => console.log("Audio bloqueado:", e));
+    moneda.style.pointerEvents = 'none';
 
-      // Estas dos líneas de código sirven para calcular el punto exacto al centro de la ranura donde debe terminar la moneda al ser soltada, asegurando que quede perfectamente alineada (tanto horizontal como verticalmente) sin importar en qué parte exacta de la ranura la haya soltado el usuario.
-      const targetX = ranuraRect.left - moneda.offsetParent.getBoundingClientRect().left + (ranuraRect.width / 2) - (monedaRect.width / 2);
-      const targetY = ranuraRect.top - moneda.offsetParent.getBoundingClientRect().top + (ranuraRect.height / 2) - (monedaRect.height / 2) + 15;
+    // Activa sonido
+    dropSound.currentTime = 0;
+    dropSound.play().catch(e => console.log("Audio bloqueado:", e));
 
-      // Animación de entrada a la ranura
-      const animacion = moneda.animate([
-        { 
-          left: `${posicionactualX}px`, 
-          top: `${posicionactualY}px`, 
-          transform: 'scale(1) rotate(0deg)',
-          opacity: 1,
-          zIndex: 10 
-        },
-        { 
-          left: `${posicionactualX}px`, 
-          top: `${posicionactualY}px`, 
-          transform: 'scale(0.5) rotate(25deg)',
-          opacity: 1,
-          zIndex: 10 
-        },
-        { 
-          left: `${targetX}px`, 
-          top: `${targetY}px`, 
-          transform: 'scaleX(0.2) scaleY(0.1) rotate(45deg)', 
-          opacity: 0, 
-          zIndex: 1 
-        }
-      ], {
-        duration: 600,
-        easing: 'cubic-bezier(0.5, 0, 1, 1)',
-        fill: 'forwards'
-      });
+    // Estas dos líneas de código sirven para calcular el punto exacto al centro de la ranura donde debe terminar la moneda al ser soltada, asegurando que quede perfectamente alineada (tanto horizontal como verticalmente) sin importar en qué parte exacta de la ranura la haya soltado el usuario.
+    const targetX = ranuraRect.left - moneda.offsetParent.getBoundingClientRect().left + (ranuraRect.width / 2) - (monedaRect.width / 2);
+    const targetY = ranuraRect.top - moneda.offsetParent.getBoundingClientRect().top + (ranuraRect.height / 2) - (monedaRect.height / 2) + 15;
 
-      animacion.onfinish = () => {
-        openMenu();
-      };
-      
+    // Animación de entrada a la ranura
+    const animacion = moneda.animate([
+      {
+        left: `${posicionactualX}px`,
+        top: `${posicionactualY}px`,
+        transform: 'scale(1) rotate(0deg)',
+        opacity: 1,
+        zIndex: 10
+      },
+      {
+        left: `${posicionactualX}px`,
+        top: `${posicionactualY}px`,
+        transform: 'scale(0.5) rotate(25deg)',
+        opacity: 1,
+        zIndex: 10
+      },
+      {
+        left: `${targetX}px`,
+        top: `${targetY}px`,
+        transform: 'scaleX(0.2) scaleY(0.1) rotate(45deg)',
+        opacity: 0,
+        zIndex: 1
+      }
+    ], {
+      duration: 600,
+      easing: 'cubic-bezier(0.5, 0, 1, 1)',
+      fill: 'forwards'
+    });
+
+    animacion.onfinish = () => {
+      openMenu();
+    };
+
   } else {
-      // Animación de regreso si cae fuera
-      moneda.animate([
-        { left: `${posicionactualX}px`, top: `${posicionactualY}px` },
-        { left: `${inicioX}px`, top: `${inicioY}px` }
-      ], {
-        duration: 300,
-        easing: 'ease-out'
-      });
+    // Animación de regreso si cae fuera
+    moneda.animate([
+      { left: `${posicionactualX}px`, top: `${posicionactualY}px` },
+      { left: `${inicioX}px`, top: `${inicioY}px` }
+    ], {
+      duration: 300,
+      easing: 'ease-out'
+    });
 
-      moneda.style.left = `${inicioX}px`;
-      moneda.style.top = `${inicioY}px`;
-      posicionactualX = inicioX;
-      posicionactualY = inicioY;
+    moneda.style.left = `${inicioX}px`;
+    moneda.style.top = `${inicioY}px`;
+    posicionactualX = inicioX;
+    posicionactualY = inicioY;
   }
 }
 
 //* Abrir menú
 function openMenu() {
-    menudesplegable.classList.add('active');
+  menudesplegable.classList.add('active');
 }
 /* --------- FIN: MENU INTERACTIVO --------- */
 
 /* --------- INICIO: footer --------- */
 const mainfooter = document.querySelector(".footer-container");
 const renderfooter = () => {
-    const renderfooter = `
+  const renderfooter = `
     <!-- Grilla principal de 4 columnas -->
     <div class="footer-grid">
 
@@ -290,7 +290,7 @@ const renderfooter = () => {
       <p>&copy; 2026 <strong class="white-text">THE PIXEL VAULT</strong>. Todos los derechos reservados.</p>
       <p class="credits">BY DEV CORE SOLUTIONS</p>
     </div>`
-    mainfooter.insertAdjacentHTML("beforeend", renderfooter);
+  mainfooter.insertAdjacentHTML("beforeend", renderfooter);
 };
 renderfooter();
 /* --------- FIN:: footer --------- */
